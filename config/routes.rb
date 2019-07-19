a6dd35c4e074894e16c7a0bc0faa7076d321dd40
kodama_t <k.tak1014@gmail.com>
2019-07-19 10:10:22 +0900

    static pages model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#result'
+  root 'static_pages#home'
+  get  '/result',    to: 'static_pages#result'
 
   resources :microposts
   resources :users
